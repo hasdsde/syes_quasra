07e51c2730420756301ce8af7d91ddf84f45e705
hasdsde <1259305706@qq.com>
2022-10-02 22:55:59 +0800

    同源！同源！

--- src/monitors/SysInfo.vue
+++ src/monitors/SysInfo.vue
@@ -1,20 +1,56 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-banner class="bg-primary text-white">
-      这是SysInfo页面
-      <template v-slot:action>
-        <q-btn flat color="white" label="Dismiss"/>
-        <q-btn flat color="white" label="Update Credit Card"/>
-      </template>
-    </q-banner>
+
+
+    <q-card class="my-card">
+      <q-card-section>
+        <div class="text-h6">概览</div>
+      </q-card-section>
+
+      <q-separator/>
+
+      <q-card-actions class="q-pa-md" vertical>
+        <div class="q-pa-md">
+          <span class="float-left">系统状态</span>
+          <span class="float-right text-green">正常</span>
+        </div>
+        <div class="q-pa-md">
+          <span class="float-left">数据库状态</span>
+          <span class="float-right text-green">正常</span>
+        </div>
+        <div class="q-pa-md">
+          <span class="float-left">缓存数据库</span>
+          <span class="float-right text-green">正常</span>
+        </div>
+        <div class="q-pa-md">
+          <span class="float-left">硬盘</span>
+          <span class="float-right text-green">正常</span>
+        </div>
+        <div class="q-pa-md">
+          <span class="float-left">消息队列</span>
+          <span class="float-right text-green">正常</span>
+        </div>
+        <div class="q-pa-md">
+          <span class="float-left">延迟状态</span>
+          <span class="float-right text-green">正常</span>
+        </div>
+      </q-card-actions>
+    </q-card>
   </div>
 </template>
 
 <script lang="ts" setup>
+import axios from "axios";
 
+axios.get('http://192.168.31.100:8000/actuator/health').then(res => {
+  console.log(res)
+})
 </script>
 
 <style scoped>
-
+.my-card {
+  width: 100%;
+  max-width: 400px;
+}
 </style>
 
