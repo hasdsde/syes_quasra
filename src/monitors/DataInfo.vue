61def2967987081f1776fc95ca498c355b7547a8
hasdsde <1259305706@qq.com>
2022-10-05 11:26:10 +0800

    数据记录完成

--- src/monitors/DataInfo.vue
+++ src/monitors/DataInfo.vue
@@ -13,7 +13,7 @@
       <q-card-actions class="q-pa-md " vertical>
         <div class="q-pa-md">
           <q-btn-toggle
-              v-model="secondModel"
+              v-model="Reload"
               spread
               class="my-custom-toggle"
               no-caps
@@ -22,10 +22,10 @@
               color="white"
               text-color="orange"
               :options="[
-          {label: '关', value: '-1'},
-          {label: '5s', value: '5'},
-          {label: '10s', value: '10'},
-          {label: '1min', value: '60'},
+          {label: '关', value: 9999999},
+          {label: '5s', value: 10},
+          {label: '20s', value: 20},
+          {label: '1min', value: 60},
         ]"
           />
         </div>
@@ -46,16 +46,16 @@
     <div class="q-pa-sm" style="width: 39vw">
       <q-table
           title="URL监控"
-          :rows="rows"
-          :columns="columns"
+          :rows="UrlRows"
+          :columns="UrlColumns"
           row-key="name"
       />
     </div>
     <div class="q-pa-sm q-ml-md" style="width: 39vw">
       <q-table
           title="Session监控"
-          :rows="rows"
-          :columns="columns"
+          :rows="SessionRows"
+          :columns="SessionColumns"
           row-key="name"
       />
     </div>
@@ -67,23 +67,41 @@
 import axios from "axios";
 import {InfoKV} from "components/models";
 import CommCard from "components/CommCard.vue";
+import {ref} from "vue";
+import {api} from "boot/axios";
 
+const Reload = ref(20)
 const ListMysql = new InfoKV()
 const ListWeb = new InfoKV()
 const ListImp = new InfoKV()
 let DataDataSource = []
 let DataWeb = []
 let DataImp = []
+let timeCount = 1
+let UrlColumns = ref([])
+let UrlRows = ref([])
+let SessionColumns = ref([])
+let SessionRows = ref([])
+//定时器
+let timer = setInterval(() => {
+  timeCount++;
+  if (timeCount % (Reload.value) == 0) {
+    loadPage()
+  }
+}, 1000)
 loadPage()
 
 function loadPage() {
   CardList()
   WebList()
   ImpList()
+  URLList()
+  SessionList()
 }
 
 //Mysql概览
 function CardList() {
+  ListMysql.list.value.splice(0, ListMysql.list.value.length)
   axios.get('http://192.168.31.99:8000/druid/datasource.json').then(res => {
     DataDataSource = res.data.Content[0]
     ListMysql.addList("获取连接时检测", DataDataSource.TestOnBorrow === true ? '开启' : '关闭')
@@ -97,6 +115,7 @@ function CardList() {
 
 //Web检测
 function WebList() {
+  ListWeb.list.value.splice(0, ListWeb.list.value.length)
   axios.get('http://192.168.31.99:8000/druid/webapp.json').then(res => {
     DataWeb = res.data.Content[0]
     ListWeb.addList("正在执行", DataWeb.RunningCount)
@@ -110,9 +129,9 @@ function WebList() {
 
 //重要SQL检测
 function ImpList() {
+  ListImp.list.value.splice(0, ListImp.list.value.length)
   axios.get('http://192.168.31.99:8000/druid/weburi.json').then(res => {
         DataImp = res.data.Content
-        console.log(DataImp)
         //这样是为了保证顺序
         ListImp.addList("登录请求", '')
         ListImp.addList("首页浏览", '')
@@ -132,139 +151,46 @@ function ImpList() {
   )
 }
 
-const secondModel = 5
-const columns = [
-  {
-    name: 'name',
-    required: true,
-    label: 'Dessert (100g serving)',
-    align: 'left',
-    field: (row: { name: any; }) => row.name,
-    format: (val: any) => `${val}`,
-    sortable: true
-  },
-  {name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true},
-  {name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true},
-  {name: 'carbs', label: 'Carbs (g)', field: 'carbs'},
-  {name: 'protein', label: 'Protein (g)', field: 'protein'},
-  {name: 'sodium', label: 'Sodium (mg)', field: 'sodium'},
-  {
-    name: 'calcium',
-    label: 'Calcium (%)',
-    field: 'calcium',
-    sortable: true,
-    sort: (a: string, b: string) => parseInt(a, 10) - parseInt(b, 10)
-  },
-  {
-    name: 'iron',
-    label: 'Iron (%)',
-    field: 'iron',
-    sortable: true,
-    sort: (a: string, b: string) => parseInt(a, 10) - parseInt(b, 10)
-  }
-]
-const rows = [
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: '8%',
-    iron: '1%'
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: '6%',
-    iron: '7%'
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: '3%',
-    iron: '8%'
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: '7%',
-    iron: '16%'
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: '0%',
-    iron: '0%'
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: '0%',
-    iron: '2%'
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: '0%',
-    iron: '45%'
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: '2%',
-    iron: '22%'
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: '12%',
-    iron: '6%'
-  }
-]
+//URL检测
+function URLList() {
+  api.get('/tablemenu/URL').then(res => {
+    UrlColumns.value.splice(0, UrlColumns.value.length)
+    res.data.forEach((data: any) => {
+      data.sortable = true
+      //@ts-ignore
+      UrlColumns.value.push(data)
+    })
+    console.log(UrlColumns)
+  })
+  axios.get('http://192.168.31.99:8000/druid/weburi.json').then(res => {
+    UrlRows.value.splice(0, UrlRows.value.length)
+    res.data.Content.forEach((data: any) => {
+      //@ts-ignore
+      UrlRows.value.push(data)
+    })
+    console.log(UrlRows)
+  })
+}
+
+//Session
+function SessionList() {
+  api.get('/tablemenu/Session').then(res => {
+    SessionColumns.value.splice(0, SessionColumns.value.length)
+    res.data.forEach((data: any) => {
+      data.sortable = true
+      //@ts-ignore
+      SessionColumns.value.push(data)
+    })
+  })
+  axios.get('http://192.168.31.99:8000/druid/websession.json').then(res => {
+    SessionRows.value.splice(0, SessionRows.value.length)
+    res.data.Content.forEach((data: any) => {
+      //@ts-ignore
+      SessionRows.value.push(data)
+    })
+  })
+}
+
 
 </script>
 
